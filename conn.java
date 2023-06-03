250aa994d80f87812150ef882b5b153c04fe0d42
Aarthi1209 <89206591+Aarthi1209@users.noreply.github.com>
2023-06-03 21:45:56 +0530

    Update conn.java

--- conn.java
+++ conn.java
@@ -13,7 +13,7 @@ public class conn{
     public conn(){  
         try{            
             Class.forName("com.mysql.cj.jdbc.Driver");
-            String url="jdbc:mysql://localhost:3306/hotelmanagementsystem"; 
+            String url="jdbc:mysql://localhost:3306/flight"; 
             c =DriverManager.getConnection("jdbc:mysql:///flight","root","Aarthipoornimaa51827");             
             s =c.createStatement();  
         }catch(Exception e){ 
